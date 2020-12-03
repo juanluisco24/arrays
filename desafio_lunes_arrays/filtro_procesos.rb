391ba813d33c40dc5e3809450b90c6a71f7f9330
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2020-12-02 21:45:06 -0300

    haciendo arreglos

--- desafio_lunes_arrays/filtro_procesos.rb
+++ desafio_lunes_arrays/filtro_procesos.rb
@@ -1,10 +1,11 @@
 def file_to_i(filename)
-    original_data = data(filename).readlines
-    original_data.map {|i| i.to_i}
+    original_data = open(filename).readlines
+    original_data.map {|i| i.to_i }
 end
 
+    
 data_i = file_to_i("procesos")
 limite = ARGV[0].to_i
-data_final = data_i.reject{|x| x < limite}
+data_final = data_i.reject {|x| x < limite}
 
-file.write('procesos_dos', data_final.join("\n"))
+File.write('procesos_filtrados.data', data_final.join("\n"))
